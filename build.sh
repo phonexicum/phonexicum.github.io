7e8089b31218f2a9ac0500d407418e04177bbaf8
phonexicum <phonexicum@yandex.ru>
2016-08-21 08:25:02 -0700

    Patched .git in static.phonexicum.github.io and changed README.md for phonexicum.github.io

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
 #! /bin/bash
-bundle exec jekyll clean && bundle exec jekyll build -t && cp -r -v static.phonexicum.github.io/. ./phonexicum.github.io/
+bundle exec jekyll clean &&
+bundle exec jekyll build -t &&
+cp -r -v static.phonexicum.github.io/. ./phonexicum.github.io/ &&
+mv ./phonexicum.github.io/_.git ./phonexicum.github.io/.git
