f779eed691888cd6ffb7bb348b0f3f682cd7de0a
phonexicum <phonexicum@yandex.ru>
2016-08-21 08:34:02 -0700

    Patched build.sh .

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@ bundle exec jekyll clean &&
 bundle exec jekyll build -t &&
 cp -r -v static.phonexicum.github.io/. ./phonexicum.github.io/ &&
 mv ./phonexicum.github.io/_.git ./phonexicum.github.io/.git &&
+mv ./phonexicum.github.io/_.gitignore ./phonexicum.github.io/.gitignore &&
+mv ./phonexicum.github.io/_.gitmodules ./phonexicum.github.io/.gitmodules &&
 cd phonexicum.github.io &&
 git commit -a --amend -m "Added some content." &&
 cd ../
